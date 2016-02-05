c863c67d34276c6b66e500f3c7d7fc32960f1899
kaycook <maxxeyxthexangel@gmail.com>
2016-02-05 03:46:57 -0500

    Order page
    Mostly created order page. Fixed a mistake on about page.

---
+++ select.js
@@ -0,0 +1,71 @@
+			var states = {
+				'AL' : 'Alabama',
+				'AK' : 'Alaska',
+				'AZ' : 'Arizona',
+				'AR' : 'Arkansas',
+				'CA' : 'California',
+				'CO' : 'Colorado',
+				'CT' : 'Connecticut',
+				'DE' : 'Delaware',
+				'FL' : 'Florida',
+				'GA' : 'Georgia',
+				'HI' : 'Hawaii',
+				'ID' : 'Idaho',
+				'IL' : 'Illinois',
+				'IN' : 'Indiana',
+				'IA' : 'Iowa',
+				'KS' : 'Kansas',
+				'KY' : 'Kentucky',
+				'LA' : 'Louisiana',
+				'ME' : 'Maine',
+				'MD' : 'Maryland',
+				'MA' : 'Massachusetts',
+				'MI' : 'Michigan',
+				'MN' : 'Minnesota',
+				'MS' : 'Mississippi',
+				'MO' : 'Missouri',
+				'MT' : 'Montana',
+				'NE' : 'Nebraska',
+				'NV' : 'Nevada',
+				'NH' : 'New Hampshire',
+				'NJ' : 'New Jersey',
+				'NM' : 'New Mexico',
+				'NY' : 'New York',
+				'NC' : 'North Carolina',
+				'ND' : 'North Dakota',
+				'OH' : 'Ohio',
+				'OK' : 'Oklahoma',
+				'OR' : 'Oregon',
+				'PA' : 'Pennsylvania',
+				'RI' : 'Rhode Island',
+				'SC' : 'South Dakota',
+				'TN' : 'Tennessee',
+				'TX' : 'Texas',
+				'UT' : 'Utah',
+				'VT' : 'Vermont',
+				'VA' : 'Virginia',
+				'WA' : 'Washington',
+				'WV' : 'West Virginia',
+				'WI' : 'Wisconsin',
+				'WY' : 'Wyoming'
+			};
+			
+var legoSet = {
+	"Adventures of Bikini Bottom" : {
+										'price' : 40.00,
+										'image' : 'AdventureOfBB.jpg',
+										'name' : "Adventures of Bikini Bottom"
+									},
+	"The Krusty Krab Adventures" : {
+										'price' : 25.00,
+										'image' : 'TheKKShack.jpg',
+										'name' : "The Krusty Krab Adventures"
+									},
+	"The Flying Dutchman" : {
+										'price' : 25.00,
+										'image' : 'TFD.jpg',
+										'name' : "The Flying Dutchman"
+									}
+};
+		
+							
